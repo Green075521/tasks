859abefaf90fb4c8b68715be21121c7e25b12e69
Nathan Green <nathan.r.green@maine.edu>
2024-09-24 18:29:25 -0400

    Implemented stringstoIntegers

--- src/arrays.ts
+++ src/arrays.ts
@@ -29,7 +29,10 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    const result = numbers.map((n: string): number =>
+        Number(n) ? Number(n) : 0,
+    );
+    return result;
 }
 
 /**
