4a316f684abf473a4c3c5e5c316fab68a50ccb9b
Nathan Green <nathan.r.green@maine.edu>
2024-10-01 12:52:40 -0400

    Implemented toMarkdown

--- src/objects.ts
+++ src/objects.ts
@@ -77,7 +77,11 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    let result: string = "# " + question.name + "\n" + question.body;
+    if (question.type === "multiple_choice_question") {
+        result += "\n- " + question.options.join("\n- ");
+    }
+    return result;
 }
 
 /**
